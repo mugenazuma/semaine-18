d365f5c2ba96d0758b3e84d3e5784813cfebca78
mugenazuma <77062440+mugenazuma@users.noreply.github.com>
2021-05-06 11:18:25 +0200

    Add files via upload

--- création table Users.php
+++ création table Users.php
@@ -4,47 +4,59 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Détail article</title>
+    <title>Création de la table </title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <?php
     require ("connexion_bdd.php"); // Inclusion de notre bibliothèque de fonctions
     $db = connexionBase("localhost","root","","jarditou"); // Appel de la fonction de connexion
     ?>
+    
+
+
 </head>
-<h1>Création de la table Users MySQL</h1>  
+<body>
+<h1>Création de la table Users</h1>  
         <?php
-            $servname = 'localhost';
-            $dbname = 'jarditou';
-            $user = 'root';
-            $pass = '';
+           
             
             try{
-                $dbco = new PDO("mysql:host=$servname;dbname=$dbname", $user, $pass);
+                $dbco = connexionBase("localhost","root","","jarditou");
                 $dbco->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
                 
                 $sql = "CREATE TABLE Users(
-                        Id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-                        Nom VARCHAR(30) NOT NULL,
-                        Prenom VARCHAR(30) NOT NULL,
-                        Adresse mail VARCHAR(70) NOT NULL,
-                        Login VARCHAR(30) NOT NULL,
-                        Mot de passe INT UNSIGNED NOT NULL,
-                        Date d'inscription date,
-                        Dernière connection utilisateurs date ;
+                        users_Id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+                        users_Name VARCHAR(30) NOT NULL,
+                        users_firstname VARCHAR(30) NOT NULL,
+                        users_mail_Address VARCHAR(70) NOT NULL,
+                        users_Login VARCHAR(30) NOT NULL,
+                        users_password INT UNSIGNED NOT NULL,
+                        users_Registration_date date,
+                        users_last_Connection date)"
+                        ;
                         
                 
                 $dbco->exec($sql);
-                echo 'La table a bien créée !';
+                echo 'La table a bien crée !';
             }
             
             catch(PDOException $e){
-              echo "Echec création table : " . $e->getMessage();
+              echo "Echec de création de table : " . $e->getMessage();
             }
         ?>
+
+
+
+
+
+
+        
+    <!--fichiers Javascript nécessaires à Bootstrap-->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
     </body>
 
-
+    </html>
 
 
 
